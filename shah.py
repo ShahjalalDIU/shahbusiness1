6a92270788a39cc44bb4fecc27ffd31e938ad841
Md.Shah-jalal <bigshopbd55@gmail.com>
2024-11-06 22:42:30 +0600

    shah.py

--- shah.py
+++ shah.py
@@ -27,7 +27,7 @@ def create_connection():
     conn = None
     try:
         conn = mysql.connector.connect(
-            host='localhost:3306',
+            host='192.250.235.46',
             user='shahbusi_shah',
             password='Shahjalal811',
             database='shahbusi_login'
